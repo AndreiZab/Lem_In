69f36ee17bd919405b9e81d98728ff0b5aacb36b
Андрей Заболотский <46747710+AndreiZab@users.noreply.github.com>
2019-06-25 14:18:12 +0300

    valid

--- src/parse_lins.c
+++ src/parse_lins.c
@@ -6,7 +6,7 @@
 /*   By: rhealitt <rhealitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 20:30:22 by rhealitt          #+#    #+#             */
-/*   Updated: 2019/06/24 17:23:31 by rhealitt         ###   ########.fr       */
+/*   Updated: 2019/06/25 14:15:25 by rhealitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,33 @@ int		ft_split_link(char *line, char **name1, char **name2)
 	return (FT_WRONG_FORMAT);
 }
 
-int		ft_parse_name(t_lemin *li, char *name1, char *name2)
+int 	ft_search_dup_link(t_room *room1, t_room *room2)
+{
+	if (!room1->input_links || !room2->input_links)
+		return (0);
+	if (room1->input_links->linked_room == room2 || room2->input_links->linked_room == room1)
+		return (1);
+	while(room1->input_links->next)
+	{
+		if (room1->input_links->linked_room == room2)
+			return (1);
+		if (room1->input_links->linked_room == room1)
+			return (1);
+		room1->input_links = room1->input_links->next;
+	}
+	while(room2->input_links->next)
+	{
+		if (room2->input_links->linked_room == room1)
+			return (1);
+		if (room2->input_links->linked_room == room2)
+			return (1);
+		room2->input_links = room2->input_links->next;
+	}
+	return (0);
+
+}
+
+int		ft_parse_linkname(t_lemin *li, char *name1, char *name2)
 {
 	t_room	*ptr1;
 	t_room	*ptr2;
@@ -75,8 +101,8 @@ int		ft_parse_name(t_lemin *li, char *name1, char *name2)
 			break;
 		ptr3 = ptr3->next;
 	}
-	if (!ptr1 || !ptr2)
-		return (FT_WRONG_FORMAT);
+	if (!ptr1 || !ptr2 || ptr1 == ptr2 || ft_search_dup_link(ptr1, ptr2))
+		return (FT_OK);
 	ft_link_set(ptr1, ptr2);
 	ft_link_set(ptr2, ptr1);
 	return (FT_OK);
@@ -96,12 +122,12 @@ int		ft_parse_links(char *line, t_lemin *li)
 		return (FT_NO_START);
 	err = ft_split_link(line, &name1, &name2);
 	if (err == FT_OK)
-		err = ft_parse_name(li, name1, name2);
+		err = ft_parse_linkname(li, name1, name2);
 	return (err);
 }
 
 /*
  рум-линк-рум не забыть поправить
  обработать дефисы в найме
- отрицательные координаты комнат проработать
+ координаты типа 0intmax+1 - краш
  */
